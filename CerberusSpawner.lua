edb8d5359f83fe0a7a622899747035268794fa73
fissurectomy <150694338+fissurectomy@users.noreply.github.com>
2024-03-07 14:52:31 +0800

    Update CerberusSpawner.lua

--- CerberusSpawner.lua
+++ CerberusSpawner.lua
@@ -1,5 +1,6 @@
 repeat task.wait() until game:IsLoaded()
 local notifs = loadstring(game:HttpGet('https://raw.githubusercontent.com/fissurectomy/open-source-code/main/notification.lua'))()
 notifs.alert('Cerberus Spawner', nil, 1000000, 'rainbow')
+task.wait(0.25)
 notifs.alert('Loading! Lag spikes are normal.', nil, 5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fissurectomy/1x1.lua/main/Cerberus.lua"))()
